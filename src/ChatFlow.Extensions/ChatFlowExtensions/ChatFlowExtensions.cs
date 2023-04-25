14f4a13eb5e4f1db64c77b8548168754834d7707
pmosk <pm@ggroupp.ru>
2023-04-25 17:14:59 +0400

    implement SendOrSkip and ReplaceOrSkip

---
+++ src/ChatFlow.Extensions/ChatFlowExtensions/ChatFlowExtensions.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+
+namespace GGroupp.Infra.Bot.Builder;
+
+public static partial class ChatFlowExtensions
+{
+    private static async Task<ResourceResponse> InnerReplaceActivityAsync(
+        this ITurnContext turnContext, IActivity activity, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(activity.Id) || (IsAllowedChannelToDelete(activity.ChannelId) is false))
+        {
+            return await turnContext.SendActivityAsync(activity, cancellationToken).ConfigureAwait(false);
+        }
+
+        var activityDeleteTask = turnContext.DeleteActivityAsync(activity.Id, cancellationToken);
+        var activitySendTask = turnContext.SendActivityAsync(activity, cancellationToken);
+
+        await Task.WhenAll(activityDeleteTask, activitySendTask).ConfigureAwait(false);
+        return await activitySendTask.ConfigureAwait(false);
+
+        static bool IsAllowedChannelToDelete(string channelId)
+            =>
+            string.Equals(channelId, Channels.Telegram, StringComparison.InvariantCultureIgnoreCase) ||
+            string.Equals(channelId, Channels.Msteams, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
---
+++ src/ChatFlow.Extensions/ChatFlowExtensions/Extensions.SendOrSkip.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Schema;
+
+namespace GGroupp.Infra.Bot.Builder;
+
+partial class ChatFlowExtensions
+{
+    public static ChatFlow<T> SendActivityOrSkip<T>(
+        this ChatFlow<T> chatFlow, Func<IChatFlowContext<T>, IActivity?> activityProvider, Func<T, ResourceResponse, T>? resultMapper = null)
+    {
+        ArgumentNullException.ThrowIfNull(chatFlow);
+        ArgumentNullException.ThrowIfNull(activityProvider);
+
+        return chatFlow.NextValue(InnerInvokeAsync);
+
+        async ValueTask<T> InnerInvokeAsync(IChatFlowContext<T> context, CancellationToken cancellationToken)
+        {
+            var activity = activityProvider.Invoke(context);
+            if (activity is null)
+            {
+                return context.FlowState;
+            }
+
+            var response = await context.SendActivityAsync(activity, cancellationToken).ConfigureAwait(false);
+            if (resultMapper is null)
+            {
+                return context.FlowState;
+            }
+
+            return resultMapper.Invoke(context.FlowState, response);
+        }
+    }
+}
